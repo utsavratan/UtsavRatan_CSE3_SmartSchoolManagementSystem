e158c3a45adebe54e225b7b78e051d9565035bf1
Utsav Ratan <2401010046@krmu.edu.in>
2025-04-29 18:47:24 +0530

    Update Results.tsx

--- src/components/sections/Results.tsx
+++ src/components/sections/Results.tsx
@@ -8,52 +8,52 @@ interface Result {
   remarks: string;
 }
 
-export const Result = () => {
-  const results: Result[] = [
-    {
-      id: 1,
-      subject: 'Mathematics',
-      score: 95,
-      grade: 'A',
-      remarks: 'Excellent',
-    },
-    {
-      id: 2,
-      subject: 'Science',
-      score: 95,
-      grade: 'A',
-      remarks: 'Excellent',
-    },
-    {
-      id: 3,
-      subject: 'English',
-      score: 92,
-      grade: 'A',
-      remarks: 'Excellent',
-    },
-    {
-      id: 4,
-      subject: 'Hindi',
-      score: 90,
-      grade: 'A',
-      remarks: 'Excellent',
-    },
-    {
-      id: 5,
-      subject: 'Computer',
-      score: 99,
-      grade: 'A',
-      remarks: 'Excellent',
-    },
-    {
-      id: 6,
-      subject: 'Social Science',
-      score: 98,
-      grade: 'A',
-      remarks: 'Excellent',
-    },
-  ];
+export const results: Result[] = [
+  {
+    id: 1,
+    subject: 'Mathematics',
+    score: 95,
+    grade: 'A',
+    remarks: 'Excellent',
+  },
+  {
+    id: 2,
+    subject: 'Science',
+    score: 95,
+    grade: 'A',
+    remarks: 'Excellent',
+  },
+  {
+    id: 3,
+    subject: 'English',
+    score: 92,
+    grade: 'A',
+    remarks: 'Excellent',
+  },
+  {
+    id: 4,
+    subject: 'Hindi',
+    score: 90,
+    grade: 'A',
+    remarks: 'Excellent',
+  },
+  {
+    id: 5,
+    subject: 'Computer',
+    score: 99,
+    grade: 'A',
+    remarks: 'Excellent',
+  },
+  {
+    id: 6,
+    subject: 'Social Science',
+    score: 98,
+    grade: 'A',
+    remarks: 'Excellent',
+  },
+];
 
+export const Result = () => {
   return (
     <div className="rounded-md border">
       <Table>
