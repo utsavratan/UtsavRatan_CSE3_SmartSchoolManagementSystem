725d0c8ddb7e9f658dfb08cf6729af769b66fc22
Utsav Ratan <2401010046@krmu.edu.in>
2025-04-29 21:24:28 +0530

    Update StudentDetails.tsx

--- src/components/sections/StudentDetails.tsx
+++ src/components/sections/StudentDetails.tsx
@@ -9,7 +9,7 @@ export const StudentDetails = () => {
     course: 'B.Tech CSE',
     address: 'New Delhi , India',
     parentsDetails: 'Mr. Rajeev Kumar and Mrs. Priyanka Kumari',
-    profileImage: 'path/to/profile-image.jpg', // Replace with actual image path
+    profileImage: '/lovable-uploads/utsav.jpeg', // Replace with actual image path
   };
 
   return (
